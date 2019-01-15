d3838d379118ae428a13206e70855370ebef0fe9
Eric Vicenti <eric@aven.io>
2019-01-14 17:11:09 -0800

    Attempt upgrade to react-native .57 (expo 32)

--- example/rn-cli.config.js
+++ example/rn-cli.config.js
@@ -1,26 +1,26 @@
 /* eslint-disable import/no-commonjs */
 
-const path = require('path');
-const glob = require('glob-to-regexp');
-const blacklist = require('metro/src/blacklist');
-const pak = require('../package.json');
-const pak2 = require('./package.json');
+const path = require("path");
+const glob = require("glob-to-regexp");
+const blacklist = require("metro-config/src/defaults/blacklist");
+const pak = require("../package.json");
+const pak2 = require("./package.json");
 
 const dependencies = Object.keys(pak.dependencies || {});
 const localDependencies = Object.keys(pak2.dependencies);
 const peerDependencies = Object.keys(pak.peerDependencies);
 
 module.exports = {
-  getProjectRoots() {
-    return [__dirname, path.resolve(__dirname, '..')];
-  },
+  watchFolders: [path.resolve(__dirname, "..")],
   getProvidesModuleNodeModules() {
     return [...dependencies, ...localDependencies, ...peerDependencies];
   },
-  getBlacklistRE() {
-    return blacklist([glob(`${path.resolve(__dirname, '..')}/node_modules/*`)]);
+  resolver: {
+    blacklistRE: blacklist([
+      glob(`${path.resolve(__dirname, "..")}/node_modules/*`)
+    ])
   },
   getSourceExts() {
-    return ['ts', 'tsx'];
-  },
+    return ["ts", "tsx"];
+  }
 };
